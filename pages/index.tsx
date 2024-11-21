92f84571a3fef70bc499bc4abf949eaa9ec06a50
motta10motta <meneo10@gmail.com>
2024-11-21 20:00:51 -0300

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,24 +1,17 @@
 import { useSession, signIn, signOut } from "next-auth/react"
-import Link from 'next/link'
 
 export default function Home() {
-  const { data: session } = useSession()
+  const { data: session, status } = useSession()
+
+  if (status === "loading") {
+    return <p>Cargando...</p>
+  }
 
   if (session) {
     return (
       <>
         Signed in as {session.user?.email} <br />
         <button onClick={() => signOut()}>Sign out</button>
-        <div>
-          <Link href="/sales">
-            <a>Ir a Ventas</a>
-          </Link>
-        </div>
-        <div>
-          <Link href="/payments">
-            <a>Ir a Pagos</a>
-          </Link>
-        </div>
       </>
     )
   }
