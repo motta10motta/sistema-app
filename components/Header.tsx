6ce6dcd0924abb8c329e26f377c939a1efbc1de7
motta10motta <meneo10@gmail.com>
2024-11-21 20:35:48 -0300

    Create Header.tsx

---
+++ components/Header.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function Header() {
+  return (
+    <header className="bg-green-600 text-white p-4">
+      <div className="container mx-auto flex justify-between items-center">
+        <h1 className="text-2xl font-bold">Sistema Cabrillana</h1>
+        <p className="text-sm">Desarrollado por GB TechX</p>
+      </div>
+    </header>
+  )
+}
+
